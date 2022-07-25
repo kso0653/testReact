4e8d2a15ed2fe5184e9efabc7bc44f2514086ae6
kso0654 <kso0653@naver.com>
2022-07-25 18:01:01 +0900

    start react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
+import Wrapper from "./Wrapper";
+import Counter from "./Counter";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch('/api/hello')
+        .then(response => response.text())
+        .then(message => {
+          setMessage(message);
+        });
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">{message}</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Wrapper>
+              <Hello name={"리액트"} color={"red"} isSpecial={true}/>
+              <Hello color={"blue"}/>
+          </Wrapper>
+          <Counter />
+      </div>
   );
 }
 
